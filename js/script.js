3ae7ac54cc2fbed98682928a2a65350975d6b517
bijdjgajiefja <47941175+bijdjgajiefja@users.noreply.github.com>
2022-09-29 10:55:28 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.your-class').slick({
+        infinite: true,
+        speed: 2000,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true
+
+    });
+});
